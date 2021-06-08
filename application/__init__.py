7d86e25eb8d37f950f115aeaee9728adf5b56f71
Yan Gianini <yangianini@gmail.com>
2021-06-08 17:30:51 -0300

    controller adcionado

--- application/__init__.py
+++ application/__init__.py
@@ -3,3 +3,6 @@ import os
 
 
 app = Flask(__name__, static_folder=os.path.abspath("application/view/static"), template_folder=os.path.abspath("application/view/templates"))
+
+
+from application.controller import main_controller
