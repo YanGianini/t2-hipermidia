bbd5442eea41fd2349fe0d7e92341002188036e4
Yan Gianini <yangianini@gmail.com>
2021-06-09 15:58:54 -0300

    função adicionar implementada e ajustes no js do form flutuante

--- application/controller/main_controller.py
+++ application/controller/main_controller.py
@@ -1,9 +1,27 @@
+from application.model.entity.ferramenta import Ferramenta
 from application import app 
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, redirect
 from application.model.dao.ferramentaDAO import FerramentaDAO
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     ferramentaDAO = FerramentaDAO()
     lista_ferramentas = ferramentaDAO.lista_ferramentas()
     return render_template("index.html",lista_ferramentas=lista_ferramentas)
+
+@app.route("/adicionar", methods=["POST"])
+def adicionar():
+    ferramentaDAO = FerramentaDAO()
+    lista_ferramentas = ferramentaDAO.lista_ferramentas()
+
+    nome = request.form.get('fer_nome', None)
+    link = request.form.get('fer_link', None)
+    desc = request.form.get('fer_desc', None)
+    tags = request.form.get('fer_tags')
+    fer = Ferramenta(nome, link, desc)
+    
+    for i in tags:
+        fer.add_tag(i)
+
+    lista_ferramentas.append(fer)
+    return redirect(url_for('index'))
---
+++ application/view/static/Scripts/scripts.js
@@ -0,0 +1,9 @@
+function abrirJanela(){
+    janela = window.document.getElementById("formulario_suspenso");
+    janela.style.display = "block";
+}
+
+function fecharJanela(){
+    janela = window.document.getElementById("formulario_suspenso");
+    janela.style.display = "None";
+}
