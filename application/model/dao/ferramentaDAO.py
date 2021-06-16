29d15ff0c784553047cef47a3354a6c62bf35ef2
Yan Gianini <yangianini@gmail.com>
2021-06-15 21:44:58 -0300

    esqueci o requeriments.txt

--- application/model/dao/ferramentaDAO.py
+++ application/model/dao/ferramentaDAO.py
@@ -2,15 +2,12 @@ from typing import List
 from application.model.entity.ferramenta import Ferramenta
 
 ferramenta1 = Ferramenta("Todo mvc", "linkzin", "mvc do professor com estrutura mvc")
-ferramenta2 = Ferramenta("T2 tassio lindo", "link lindo", "nosso lindo professor pediu para que façamos trabalho liindo")
 
 ferramenta1.add_tag("tarefa")
 ferramenta1.add_tag("organização")
 
-ferramenta2.add_tag("beleza")
-ferramenta2.add_tag("diverção")
 
-ferramenta_list = [ferramenta1, ferramenta2]
+ferramenta_list = [ferramenta1]
 
 class FerramentaDAO():
     def __init__(self):
